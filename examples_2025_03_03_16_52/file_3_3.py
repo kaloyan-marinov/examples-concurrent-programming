de2d9603bfeaa628211c6c59b8cc2e87f659931c
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2025-03-03 17:14:50 +0100

    add `examples_2025_03_03_16_52/file_3_3.py`

---
+++ examples_2025_03_03_16_52/file_3_3.py
@@ -0,0 +1,39 @@
+"""
+examples-concurrent-programming $ PYTHONPATH=. \
+    python3 \
+    examples_2025_03_03_16_52/file_3_3.py 
+starting to sleep for 1.3 second(s)
+slept for 1.3 second(s)
+the execution of the main script finished after 1.73 seconds(s)
+"""
+
+import concurrent.futures
+import time
+
+from examples_2025_03_03_15_36.file_1 import do_something
+
+
+if __name__ == "__main__":
+
+    time_start = time.perf_counter()
+    with concurrent.futures.ProcessPoolExecutor() as p_p_e:
+        # Schedule a function to be executed.
+        # The returned value is a "future" object;
+        # that object enables us to check in on the execution of our function
+        # _after_ it has been scheduled;
+        # in particular, that object enables us to check
+        # [if] it's running, or if it's done, and also to check the result
+        f = p_p_e.submit(do_something, 1.3)
+
+        # The next call causes it to wait aroud
+        # until the scheduled function completes.
+        r = f.result()
+
+        print(r)
+
+    time_final = time.perf_counter()
+
+    main_script_duration = round(time_final - time_start, 2)
+    print(
+        f"the execution of the main script finished after {main_script_duration} seconds(s)"
+    )
