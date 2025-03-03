a27074483229c31fb1f0f46942600349379d0ac2
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2025-03-03 16:26:14 +0100

    add `examples_2025_03_03_15_36/file_3_4.py`

---
+++ examples_2025_03_03_15_36/file_3_4.py
@@ -0,0 +1,24 @@
+import concurrent.futures
+import time
+
+from file_1 import do_something
+
+
+if __name__ == "__main__":
+
+    time_start = time.perf_counter()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as t_p_e:
+        # Schedule several executions of a function.
+        futures = [t_p_e.submit(do_something, 1.4) for _ in range(10)]
+
+        # The following iterator yields the results of the threads,
+        # as they are completed.
+        for f in concurrent.futures.as_completed(futures):
+            print(f.result())
+
+    time_final = time.perf_counter()
+
+    main_script_duration = round(time_final - time_start, 2)
+    print(
+        f"the execution of the main script finished after {main_script_duration} seconds(s)"
+    )
