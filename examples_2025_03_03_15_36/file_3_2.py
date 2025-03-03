1d670076e120cbe80991a8927f6f14c59abce4f0
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2025-03-03 16:12:10 +0100

    add `examples_2025_03_03_15_36/file_3_2.py`

---
+++ examples_2025_03_03_15_36/file_3_2.py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+from file_1 import do_something
+
+
+if __name__ == "__main__":
+    time_start = time.perf_counter()
+
+    threads = []
+    for _ in range(10):
+        t = threading.Thread(
+            target=do_something,
+            args=(1,),
+        )
+        t.start()
+        threads.append(t)
+
+    # Enforce that both `Thread`s should finish executing
+    # before the Python interpreter goes on to execute the rest of the main script
+    for t in threads:
+        t.join()
+
+    time_final = time.perf_counter()
+
+    main_script_duration = round(time_final - time_start, 2)
+    print(
+        f"the execution of the main script finished after {main_script_duration} seconds(s)"
+    )
